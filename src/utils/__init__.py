2ed484139cedbe8e162c4e194b162d517e3bcbb1
Bayilla Geda <bayillag@gmail.com>
2026-07-07 04:39:02 +0300

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,9 @@
+# src/utils/__init__.py
 
+"""
+Surveillance database utility configurations, biophysical range limits, 
+and priority pathogen parameter registries.
+"""
+
+from .disease_profiles import DISEASE_PATHOGEN_REGISTRY
+from .parameter_registry import ENVIRONMENTAL_PARAMETER_REGISTRY
